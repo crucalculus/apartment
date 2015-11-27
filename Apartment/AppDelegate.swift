417a0e2a6d4d9dfb4660d0876415f9854e5ce296
Eric Abell <crucalculus@gmail.com>
2015-11-27 07:51:16 -0600

    Added multiple beacons and an Exit Message.
    I'm hoping that by selecting beacons only by UUID (I have 4 in the apartment), this will eliminate the spurious enter messages that I was getting before.
    I've also added a method to receive Exit events and send a notificaiton.

--- Apartment/AppDelegate.swift
+++ Apartment/AppDelegate.swift
@@ -23,9 +23,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, ESTBeaconManagerDelegate 
         UIApplication.sharedApplication().registerUserNotificationSettings(
             UIUserNotificationSettings(forTypes: .Alert, categories: nil))
         
+        
+        // I'm only specifying the UUID -- which I think means that all 4 beacons in
+        // the apartment will serve as a *REGION* and I'm hoping this eliminates the
+        // repeating "enter" messages I'm getting...
+        
         self.beaconManager.startMonitoringForRegion(CLBeaconRegion(
             proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!,
-            major: 65344 , minor: 50164 , identifier: "monitored region"));
+            /*major: 65344 , minor: 50164 ,*/ identifier: "apartment"
+            ) );
         
         return true
     }
@@ -37,8 +43,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, ESTBeaconManagerDelegate 
             "It looks like you just walked into your " +
             "apartment. "
         UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+        print("Enter Region called.")
     }
 
+    func beaconManager(manager: AnyObject, didExitRegion region: CLBeaconRegion) {
+        let notification = UILocalNotification()
+        notification.alertBody =
+            "Goodbye Eric!" +
+            "It looks like you just left your " +
+            "apartment. "
+        UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+        print("Exit Region called.")
+    }
+
+    
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
